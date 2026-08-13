8f48098c578f449f4adea7f41bb967aee2a97d58
David Perez <perez.david3001@gmail.com>
2026-08-12 22:48:21 -0500

    Create a home route for the root path and add to routes

--- startup/routes.js
+++ startup/routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const genres = require("../routes/genres");
+const home = require("../routes/home");
 const customers = require("../routes/customers");
 const movies = require("../routes/movies");
 const rentals = require("../routes/rentals");
@@ -10,6 +11,7 @@ const error = require("../middleware/error");
 
 module.exports = function(app) {
     app.use(express.json());
+    app.use("/", home);
     app.use("/api/genres", genres);
     app.use("/api/customers", customers);
     app.use("/api/movies", movies);
